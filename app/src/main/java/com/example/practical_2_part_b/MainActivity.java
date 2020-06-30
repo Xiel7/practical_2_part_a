c2b57949f43db91844a0deeb7b78c305e2547fa6
Xiel7 <ericng402@gmail.com>
2020-06-30 18:13:03 +0800

    Practical 2 Part B

--- app/src/main/java/com/example/practical_2_part_b/MainActivity.java
+++ app/src/main/java/com/example/practical_2_part_b/MainActivity.java
@@ -1,9 +1,11 @@
-package com.example.practical_2_part_a;
+package com.example.practical_2_part_b;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    Button zeroColor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +36,27 @@ public class MainActivity extends AppCompatActivity {
         {
             mShowCount.setText(Integer.toString(mCount));
         }
+        if(mCount % 2 == 0)
+        {
+            view.setBackgroundColor(Color.MAGENTA);
+        }
+        else
+            view.setBackgroundColor(Color.GREEN);
+
+        if(mCount != 0)
+        {
+            zeroColor = (Button) findViewById(R.id.button_zero);
+            zeroColor.setBackgroundColor(Color.BLUE);
+        }
+    }
+
+    public void resetZero(View view)
+    {
+        mCount = 0;
+        mShowCount.setText(Integer.toString(mCount));
+
+        if(mCount == 0)
+            view.setBackgroundColor(Color.GRAY);
 
 
     }
